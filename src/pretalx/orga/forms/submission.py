2ae6d49be32b8d31276526ff14cbee1206ffe70a
Dirk Engling <erdgeist@erdgeist.org>
2024-10-25 19:23:22 +0200

    38C3 hack in lack of a plugin

--- src/pretalx/orga/forms/submission.py
+++ src/pretalx/orga/forms/submission.py
@@ -212,6 +212,7 @@ class SubmissionForm(ReadOnlyFlag, RequestRequire, forms.ModelForm):
             "slot_count",
             "image",
             "is_featured",
+            "ticket_id",
         ]
         widgets = {
             "tags": EnhancedSelectMultiple(color_field="color"),
